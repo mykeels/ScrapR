7f7b77c53a19c6a5a244d84b7c7ae323f26f93f1
mykeels <mykehell123@gmail.com>
2016-12-02 12:27:11 +0100

    added More Tests

--- ScrapR.Tests/WknTests.cs
+++ ScrapR.Tests/WknTests.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using ScrapR.Models;
+using ScrapR.Models.Wkn;
 using System.Diagnostics;
 using System.Windows.Forms;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -13,6 +16,34 @@ namespace ScrapR.Tests
     [TestClass]
     public class WknTests
     {
+        [TestMethod]
+        public void testOneWay()
+        {
+            Models.Wkn.Scrapper scrapper = new Models.Wkn.Scrapper();
+            Models.Wkn.Query query = Query.GetSampleQuery();
+            query.trip = Query.TripType.OneWay;
+            Roots roots = scrapper.GetFlightsData(query);
+            Assert.IsNotNull(roots);
+            Assert.AreNotEqual(roots.Count, 0);
+            CollectionAssert.AllItemsAreNotNull(roots);
+        }
 
+        [TestMethod]
+        public void testReturnTrip()
+        {
+            Models.Wkn.Scrapper scrapper = new Models.Wkn.Scrapper();
+            Models.Wkn.Query query = Query.GetSampleQuery();
+            query.trip = Query.TripType.Return;
+            Roots roots = scrapper.GetFlightsData(query);
+            Assert.IsNotNull(roots);
+            Assert.AreNotEqual(roots.Count, 0);
+            CollectionAssert.AllItemsAreNotNull(roots);
+        }
+
+        [TestMethod]
+        public void testMultiTrip()
+        {
+
+        }
     }
 }
---
+++ ScrapR.Tests/trvStartTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ScrapR.Models;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScrapR.Tests
+{
+    [TestClass]
+    class trvStartTests
+    {
+    }
+}
